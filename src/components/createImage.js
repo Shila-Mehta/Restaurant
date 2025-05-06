49f9fe3e54d5072eed5f4b4d8b1ae49e08af8f91
Nimra Jabbar <nimraabduljabbar14@gmail.com>
2025-05-07 00:28:05 +0500

    Deployment commit

--- src/components/createImage.js
+++
@@ -1,7 +0,0 @@
-export default function createImage(src,alt='',className=''){
-  const img=document.createElement('img');
-  img.src=src;
-  img.alt=alt;
-  if(className) img.className=className;
-  return img;
-}
--- src/components/createMap.js
+++
@@ -1,7 +0,0 @@
-import createElement from "./createElement";
-export default function createMap(src){
-const map=createElement('iframe','map');
-map.src=src;
-return map;
-
-}
